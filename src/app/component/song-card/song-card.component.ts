c4c0b63004b7d77563969d9392569fcbdc3cbe73
Kushan Yasiru <kushanyasiruwalpola@gmail.com>
2024-03-20 08:18:44 +0530

    Update song-card.component.ts

--- src/app/component/song-card/song-card.component.ts
+++ src/app/component/song-card/song-card.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-song-card',
   templateUrl: './song-card.component.html',
   styleUrls: ['./song-card.component.scss']
 })
-export class SongCardComponent {
+export class SongCardComponent implements OnInit {
+
+  @Input() public playlistThambnail!: string;
+  @Input() public title!: string;
+  @Input() public description!: string;
+
+  constructor(){}
+
+  ngOnInit(): void {
+  
+  }
 
 }
